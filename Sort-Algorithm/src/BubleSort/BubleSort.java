b0f1406b593ed06e203a70a72cde26dcdee67366
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-17 15:34:32 +0700

    update BubleSort

--- Sort-Algorithm/src/BubleSort/BubleSort.java
+++ Sort-Algorithm/src/BubleSort/BubleSort.java
@@ -5,19 +5,49 @@ import java.util.Arrays;
 public class BubleSort {
     static void main(String[] args) {
         int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
-        bubleSort(list);
+//        bubleSort(list);
         System.out.println(Arrays.toString(list));
+        System.out.println("\nBegin sort processing...");
+        bubbleSortByStep(list);
     }
 
     static void bubleSort(int[] list) {
         for (int i = 0; i < list.length; i++) {
             for (int j = list.length - 1; j > i; j--) {
-                if (list[j] < list[j - 1]){
+                if (list[j] < list[j - 1]) {
                     int temp = list[j];
                     list[j] = list[j - 1];
                     list[j - 1] = temp;
                 }
             }
+        }
+    }
+
+    public static void bubbleSortByStep(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    System.out.println("Swap " + list[i] + " with " + list[i + 1]);
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true; /* Next pass still needed */
+                }
+            }
+            /* Array may be sorted and next pass not needed */
+            if (needNextPass == false) {
+                System.out.println("Array may be sorted and next pass not needed");
+                break;
+            }
+            /* Show the list after sort */
+            System.out.print("List after the  " + k + "' sort: ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
         }
     }
 }
